1166743656a7dab91d359c99096a19bdf8d4587e
Crysty <392420488@qq.com>
2016-06-21 11:52:58 +0800

    class

---
+++ src/main/java/com/litecoding/smali2java/parser/clazz/Rule_classSuper.java
@@ -0,0 +1,169 @@
+/* -----------------------------------------------------------------------------
+ * Rule_classSuper.java
+ * -----------------------------------------------------------------------------
+ *
+ * Producer : com.parse2.aparse.Parser 2.3
+ * Produced : Fri Apr 12 10:40:21 MUT 2013
+ *
+ * -----------------------------------------------------------------------------
+ */
+
+package com.litecoding.smali2java.parser.clazz;
+
+import java.util.ArrayList;
+
+import com.litecoding.smali2java.entity.smali.Rule_dirSuper;
+import com.litecoding.smali2java.parser.ParserContext;
+import com.litecoding.smali2java.parser.Rule;
+import com.litecoding.smali2java.parser.Visitor;
+import com.litecoding.smali2java.parser.text.Rule_CRLF;
+import com.litecoding.smali2java.parser.text.whitespacecharacters.Rule_optPadding;
+import com.litecoding.smali2java.parser.text.whitespacecharacters.Rule_padding;
+
+final public class Rule_classSuper extends Rule
+{
+  private Rule_classSuper(String spelling, ArrayList<Rule> rules)
+  {
+    super(spelling, rules);
+  }
+
+  public Object accept(Visitor visitor)
+  {
+    return visitor.visit(this);
+  }
+
+  public static Rule_classSuper parse(ParserContext context)
+  {
+    context.push("classSuper");
+
+    boolean parsed = true;
+    int s0 = context.index;
+    ArrayList<Rule> e0 = new ArrayList<Rule>();
+    Rule rule;
+
+    parsed = false;
+    if (!parsed)
+    {
+      {
+        ArrayList<Rule> e1 = new ArrayList<Rule>();
+        int s1 = context.index;
+        parsed = true;
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_optPadding.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_dirSuper.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_padding.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_className.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_optPadding.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+        {
+          boolean f1 = true;
+          int c1 = 0;
+          for (int i1 = 0; i1 < 1 && f1; i1++)
+          {
+            rule = Rule_CRLF.parse(context);
+            if ((f1 = rule != null))
+            {
+              e1.add(rule);
+              c1++;
+            }
+          }
+          parsed = c1 == 1;
+        }
+        if (parsed)
+          e0.addAll(e1);
+        else
+          context.index = s1;
+      }
+    }
+
+    rule = null;
+    if (parsed)
+      rule = new Rule_classSuper(context.text.substring(s0, context.index), e0);
+    else
+      context.index = s0;
+
+    context.pop("classSuper", parsed);
+
+    return (Rule_classSuper)rule;
+  }
+
+	public static void main(String[] args) {
+		ParserContext context = new ParserContext(".super Landroid/app/Service;\n", true);
+		Rule rule = parse(context);
+		System.out.println("rule: " + rule);
+	}
+}
+
+/* -----------------------------------------------------------------------------
+ * eof
+ * -----------------------------------------------------------------------------
+ */
