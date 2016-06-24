1d32ab04d36e4db9031da10408f069628a5e04de
Crysty <392420488@qq.com>
2016-06-24 11:53:14 +0800

    ref

--- src/main/java/com/litecoding/smali2java/entity/smali/ref/MethodRef.java
+++ src/main/java/com/litecoding/smali2java/entity/smali/ref/MethodRef.java
@@ -5,67 +5,59 @@ import java.util.List;
 
 import com.litecoding.smali2java.entity.smali.Param;
 
-public class MethodRef extends SmaliCodeEntity
-{
-	protected String className = "";	
-	protected boolean isConstructor = false;
-	protected List<Param> params = new LinkedList<Param>(); 
-	protected String returnType = "";
-	
-	public String getClassName()
-	{
+public final class MethodRef extends SmaliCodeEntity {
+
+	private String className;
+	private boolean isConstructor = false;
+	private List<Param> params = new LinkedList<Param>();
+	private String returnType;
+
+	public String getClassName() {
 		return className;
 	}
-	
-	public void setClassName(String className)
-	{
+
+	public void setClassName(String className) {
 		this.className = className;
 	}
-	
-	public boolean isConstructor()
-	{
+
+	public boolean isConstructor() {
 		return isConstructor;
 	}
-	
-	public void setConstructor(boolean isConstructor)
-	{
+
+	public void setConstructor(boolean isConstructor) {
 		this.isConstructor = isConstructor;
 	}
-	
-	public List<Param> getParams()
-	{
+
+	public List<Param> getParams() {
 		return params;
 	}
-	
-	public void addParam(Param param)
-	{
+
+	public void addParam(Param param) {
 		this.params.add(param);
 	}
-	
-	public String getReturnType()
-	{
+
+	public String getReturnType() {
 		return returnType;
 	}
-	
-	public void setReturnType(String returnType)
-	{
+
+	public void setReturnType(String returnType) {
 		this.returnType = returnType;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append(className);
 		builder.append(" -> ");
-		
-		if(isConstructor) {
+
+		if (isConstructor) {
 			builder.append("(constructor) ");
 		}
-		
+
 		builder.append(name);
-		
+
 		builder.append(" ( ");
-		for(Param param : params) {
+		for (Param param : params) {
 			builder.append(param.toString());
 			builder.append(" ");
 		}
