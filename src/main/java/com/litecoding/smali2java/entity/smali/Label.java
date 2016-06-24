2da31774796d3610978b3e9c52689c1f01eb98c2
Crysty <392420488@qq.com>
2016-06-24 11:00:52 +0800

    smali entity

--- src/main/java/com/litecoding/smali2java/entity/smali/Label.java
+++
@@ -1,9 +0,0 @@
-package com.litecoding.smali2java.entity.smali;
-
-public class Label extends SmaliCodeEntity
-{
-	@Override
-	public String toString() {
-		return name;
-	}
-}
--- src/main/java/com/litecoding/smali2java/entity/smali/Value.java
+++
@@ -1,9 +0,0 @@
-package com.litecoding.smali2java.entity.smali;
-
-public class Value extends SmaliCodeEntity
-{
-	@Override
-	public String toString() {
-		return name;
-	}
-}
