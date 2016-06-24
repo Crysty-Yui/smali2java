69af95464636ea8114da71176ce0ad666506cd96
Crysty <392420488@qq.com>
2016-06-24 10:59:13 +0800

    ref

---
+++ src/main/java/com/litecoding/smali2java/entity/smali/ref/FieldRef.java
@@ -0,0 +1,36 @@
+package com.litecoding.smali2java.entity.smali.ref;
+
+public final class FieldRef extends SmaliCodeEntity {
+
+	private String className = "";
+	private String type = "";
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+
+		builder.append(className);
+		builder.append(" -> ");
+		builder.append(name);
+		builder.append(":");
+		builder.append(type);
+
+		return builder.toString();
+	}
+}
---
+++ src/main/java/com/litecoding/smali2java/entity/smali/ref/Value.java
@@ -0,0 +1,9 @@
+package com.litecoding.smali2java.entity.smali.ref;
+
+public class Value extends SmaliCodeEntity
+{
+	@Override
+	public String toString() {
+		return name;
+	}
+}
