d7162d1db35e4a8d476b24c6b53184a3282e7b6b
Crysty <392420488@qq.com>
2016-06-24 10:48:06 +0800

    java renderer

--- src/main/java/com/litecoding/smali2java/renderer/JavaRenderer.java
+++ src/main/java/com/litecoding/smali2java/renderer/JavaRenderer.java
@@ -4,12 +4,15 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.litecoding.smali2java.entity.java.Comment;
 import com.litecoding.smali2java.entity.java.MethodCall;
 import com.litecoding.smali2java.entity.java.Renderable;
 import com.litecoding.smali2java.entity.java.Variable;
+import com.litecoding.smali2java.entity.smali.FieldRef;
 import com.litecoding.smali2java.entity.smali.Instruction;
 import com.litecoding.smali2java.entity.smali.MethodRef;
 import com.litecoding.smali2java.entity.smali.Register;
@@ -32,7 +35,7 @@ public class JavaRenderer {
 			System.out.println("===[END OF BLOCK CHAIN]===");
 			System.out.println();
 		} catch(Exception e) {
-			System.out.println("Exception while generating block");
+			System.err.println("Exception while generating block");
 			e.printStackTrace();
 			
 			StringWriter sw = new StringWriter();
@@ -52,16 +55,146 @@ public class JavaRenderer {
 			return entities;
 		}
 		
+		// 构造方法
 		if(smaliMethod.isConstructor()) {
 			entities.addAll(generateConstructor(smaliMethod, block));
 			return entities;
 		}
 		
-		entities.add(new Comment("Can't render this method"));
+		StringBuilder sb = new StringBuilder();
+		Map<String, String> ii = new HashMap<String, String>();
+		for (Instruction ins : block.instructions) {
+			if ("iget".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				ii.put(args.get(0).getName(), args.get(2).getName());
+				if (block.isEndsByReturn) {
+					sb.append("return ");
+					sb.append(ii.get("v0"));
+					sb.append(";");
+					sb.append("\n");
+				}
+			}
+			else if ("iget-object".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				ii.put(args.get(0).getName(), args.get(2).getName());
+				if (block.isEndsByReturn) {
+					sb.append("return ");
+					sb.append(ii.get("v0"));
+					sb.append(";");
+					sb.append("\n");
+				}
+			}
+			else if ("sget-object".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				ii.put(args.get(0).getName(), JavaRenderUtils.renderShortJavaClassName(((FieldRef)args.get(1)).getClassName()) + "." + args.get(1).getName());
+				System.out.println("?????????????????????????????????????????????????????????????????");
+				System.out.println(((FieldRef)args.get(1)).getClassName() + "." + args.get(1).getName());
+			}
+			else if ("const-string".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				ii.put(args.get(0).getName(), args.get(1).getName());
+				System.out.println("################################################################");
+				System.out.println(args.get(1).getName());
+			}
+			else if ("iput".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				if (((FieldRef)args.get(2)).getClassName().equals(smaliMethod.getSmaliClass().getClassName())) {
+					sb.append("this.");
+				}
+				sb.append(args.get(2).getName());
+				sb.append(" = ");
+				sb.append(";");
+			}
+			else if ("iput-object".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				if (((FieldRef)args.get(2)).getClassName().equals(smaliMethod.getSmaliClass().getClassName())) {
+					sb.append("this.");
+				}
+				sb.append(args.get(2).getName());
+				sb.append(" = ");
+				sb.append(";");
+			}
+			else if ("invoke-virtual".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				RegisterGroup rg = (RegisterGroup)args.get(0);
+				List<SmaliCodeEntity> ens = rg.getArguments();
+//				sb.append(JavaRenderUtils.renderShortJavaClassName(((MethodRef) args.get(1)).getClassName()));
+				String v = ens.get(0).getName();
+				if (!"p0".equals(v)) {// 本类方法，省略对象
+					sb.append(ii.get(v));
+					sb.append(".");
+				}
+				sb.append(((MethodRef) args.get(1)).getName());
+				sb.append("(");
+				for (int i = 1; i < ens.size(); i++) {
+					sb.append(ii.get(ens.get(i).getName()));
+					if (i < ens.size() - 1) {
+						sb.append(", ");
+					}
+				}
+				sb.append(");");
+				sb.append("\n");
+			}
+			else if ("invoke-direct".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				RegisterGroup rg = (RegisterGroup)args.get(0);
+				List<SmaliCodeEntity> ens = rg.getArguments();
+//				sb.append(JavaRenderUtils.renderShortJavaClassName(((MethodRef) args.get(1)).getClassName()));
+				String v = ens.get(0).getName();
+				if (!"p0".equals(v)) {// 本类方法，省略对象
+					sb.append(ii.get(v));
+					sb.append(".");
+				}
+				sb.append(((MethodRef) args.get(1)).getName());
+				sb.append("(");
+				for (int i = 1; i < ens.size(); i++) {
+					sb.append(ii.get(ens.get(i).getName()));
+					if (i < ens.size() - 1) {
+						sb.append(", ");
+					}
+				}
+				sb.append(");");
+				sb.append("\n");
+			}
+			else if ("invoke-static".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				sb.append(JavaRenderUtils.renderShortJavaClassName(((MethodRef) args.get(1)).getClassName()));
+				sb.append(".(");
+				RegisterGroup rg = (RegisterGroup)args.get(0);
+				List<SmaliCodeEntity> ens = rg.getArguments();
+				for (int i = 0; i < ens.size(); i++) {
+					sb.append(ii.get(ens.get(i).getName()));
+					if (i < ens.size() - 1) {
+						sb.append(", ");
+					}
+				}
+				sb.append(");");
+				sb.append("\n");
+			}
+			else if ("invoke-super".equals(ins.getName())) {
+				List<SmaliCodeEntity> args = ins.getArguments();
+				sb.append("super.");
+				sb.append(((MethodRef) args.get(1)).getName());
+				sb.append("(");
+				RegisterGroup rg = (RegisterGroup)args.get(0);
+				List<SmaliCodeEntity> ens = rg.getArguments();
+				for (int i = 1; i < ens.size(); i++) {
+					sb.append(ii.get(ens.get(i).getName()));
+					if (i < ens.size() - 1) {
+						sb.append(", ");
+					}
+				}
+				sb.append(");");
+				sb.append("\n");
+			}
+		}
+		entities.add(new Comment(sb.toString()));
+//		entities.add(new Comment("Can't render this method"));
 		
 		return entities;
 	}
 
+	// 生成构造方法
 	private static Collection<Renderable> generateConstructor(
 			SmaliMethod smaliMethod, SmaliBlock rootBlock) {
 		List<Renderable> entities = new ArrayList<Renderable>();
