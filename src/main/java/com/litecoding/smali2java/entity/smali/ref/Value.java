cbb5c71e015b78203ff0edc7288383dc78af9154
Crysty <392420488@qq.com>
2016-06-29 11:09:48 +0800

    value

--- src/main/java/com/litecoding/smali2java/entity/smali/ref/Value.java
+++ src/main/java/com/litecoding/smali2java/entity/smali/ref/Value.java
@@ -1,7 +1,7 @@
 package com.litecoding.smali2java.entity.smali.ref;
 
-public class Value extends SmaliCodeEntity
-{
+public class Value extends SmaliCodeEntity {
+
 	@Override
 	public String toString() {
 		return name;
