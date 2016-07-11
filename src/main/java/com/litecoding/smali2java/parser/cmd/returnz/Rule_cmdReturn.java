45f0532735d32e2b4568407f8ffe76cd16b8e30f
Crysty <392420488@qq.com>
2016-07-11 16:53:33 +0800

    return

--- src/main/java/com/litecoding/smali2java/parser/cmd/returnz/Rule_cmdReturn.java
+++ src/main/java/com/litecoding/smali2java/parser/cmd/returnz/Rule_cmdReturn.java
@@ -22,200 +22,170 @@ import com.litecoding.smali2java.parser.smali.Rule_optPadding;
 import com.litecoding.smali2java.parser.smali.Rule_padding;
 import com.litecoding.smali2java.parser.text.Rule_CRLF;
 
-final public class Rule_cmdReturn extends Rule
-{
-  private Rule_cmdReturn(String spelling, ArrayList<Rule> rules)
-  {
-    super(spelling, rules);
-  }
+final public class Rule_cmdReturn extends Rule {
+	private Rule_cmdReturn(String spelling, ArrayList<Rule> rules) {
+		super(spelling, rules);
+	}
 
-  public Object accept(Visitor visitor)
-  {
-    return visitor.visit(this);
-  }
+	public Object accept(Visitor visitor) {
+		return visitor.visit(this);
+	}
 
-  public static Rule_cmdReturn parse(ParserContext context)
-  {
-    context.push("cmdReturn");
+	public static Rule_cmdReturn parse(ParserContext context) {
+		context.push("cmdReturn");
 
-    boolean parsed = true;
-    int s0 = context.index;
-    ArrayList<Rule> e0 = new ArrayList<Rule>();
-    Rule rule;
+		boolean parsed = true;
+		int s0 = context.index;
+		ArrayList<Rule> e0 = new ArrayList<Rule>();
+		Rule rule;
 
-    parsed = false;
-    if (!parsed)
-    {
-      {
-        ArrayList<Rule> e1 = new ArrayList<Rule>();
-        int s1 = context.index;
-        parsed = true;
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Rule_optPadding.parse(context);
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Terminal_StringValue.parse(context, "return");
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Rule_padding.parse(context);
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Rule_codeRegisterRet.parse(context);
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Rule_optPadding.parse(context);
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          @SuppressWarnings("unused")
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            int g1 = context.index;
-            parsed = false;
-            if (!parsed)
-            {
-              {
-                ArrayList<Rule> e2 = new ArrayList<Rule>();
-                int s2 = context.index;
-                parsed = true;
-                if (parsed)
-                {
-                  boolean f2 = true;
-                  int c2 = 0;
-                  for (int i2 = 0; i2 < 1 && f2; i2++)
-                  {
-                    rule = Rule_padding.parse(context);
-                    if ((f2 = rule != null))
-                    {
-                      e2.add(rule);
-                      c2++;
-                    }
-                  }
-                  parsed = c2 == 1;
-                }
-                if (parsed)
-                {
-                  boolean f2 = true;
-                  int c2 = 0;
-                  for (int i2 = 0; i2 < 1 && f2; i2++)
-                  {
-                    rule = Rule_commentSequence.parse(context);
-                    if ((f2 = rule != null))
-                    {
-                      e2.add(rule);
-                      c2++;
-                    }
-                  }
-                  parsed = c2 == 1;
-                }
-                if (parsed)
-                  e1.addAll(e2);
-                else
-                  context.index = s2;
-              }
-            }
-            f1 = context.index > g1;
-            if (parsed) c1++;
-          }
-          parsed = true;
-        }
-        if (parsed)
-        {
-          boolean f1 = true;
-          int c1 = 0;
-          for (int i1 = 0; i1 < 1 && f1; i1++)
-          {
-            rule = Rule_CRLF.parse(context);
-            if ((f1 = rule != null))
-            {
-              e1.add(rule);
-              c1++;
-            }
-          }
-          parsed = c1 == 1;
-        }
-        if (parsed)
-          e0.addAll(e1);
-        else
-          context.index = s1;
-      }
-    }
+		parsed = false;
+		if (!parsed) {
+			{
+				ArrayList<Rule> e1 = new ArrayList<Rule>();
+				int s1 = context.index;
+				parsed = true;
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Rule_optPadding.parse(context);
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Terminal_StringValue.parse(context, "return");
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Rule_padding.parse(context);
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Rule_codeRegisterRet.parse(context);
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Rule_optPadding.parse(context);
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					@SuppressWarnings("unused")
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						int g1 = context.index;
+						parsed = false;
+						if (!parsed) {
+							{
+								ArrayList<Rule> e2 = new ArrayList<Rule>();
+								int s2 = context.index;
+								parsed = true;
+								if (parsed) {
+									boolean f2 = true;
+									int c2 = 0;
+									for (int i2 = 0; i2 < 1 && f2; i2++) {
+										rule = Rule_padding.parse(context);
+										if (f2 = rule != null) {
+											e2.add(rule);
+											c2++;
+										}
+									}
+									parsed = c2 == 1;
+								}
+								if (parsed) {
+									boolean f2 = true;
+									int c2 = 0;
+									for (int i2 = 0; i2 < 1 && f2; i2++) {
+										rule = Rule_commentSequence.parse(context);
+										if (f2 = rule != null) {
+											e2.add(rule);
+											c2++;
+										}
+									}
+									parsed = c2 == 1;
+								}
+								if (parsed)
+									e1.addAll(e2);
+								else
+									context.index = s2;
+							}
+						}
+						f1 = context.index > g1;
+						if (parsed)
+							c1++;
+					}
+					parsed = true;
+				}
+				if (parsed) {
+					boolean f1 = true;
+					int c1 = 0;
+					for (int i1 = 0; i1 < 1 && f1; i1++) {
+						rule = Rule_CRLF.parse(context);
+						if (f1 = rule != null) {
+							e1.add(rule);
+							c1++;
+						}
+					}
+					parsed = c1 == 1;
+				}
+				if (parsed)
+					e0.addAll(e1);
+				else
+					context.index = s1;
+			}
+		}
 
-    rule = null;
-    if (parsed)
-      rule = new Rule_cmdReturn(context.text.substring(s0, context.index), e0);
-    else
-      context.index = s0;
+		rule = null;
+		if (parsed)
+			rule = new Rule_cmdReturn(context.text.substring(s0, context.index), e0);
+		else
+			context.index = s0;
 
-    context.pop("cmdReturn", parsed);
+		context.pop("cmdReturn", parsed);
 
-    return (Rule_cmdReturn)rule;
-  }
+		return (Rule_cmdReturn) rule;
+	}
 }
 
-/* -----------------------------------------------------------------------------
+/*
+ * -----------------------------------------------------------------------------
  * eof
  * -----------------------------------------------------------------------------
  */
